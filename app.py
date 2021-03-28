35814978f1791c506bc457642b48a85570e4dab6
inpruner <aerokhov@gmail.com>
2021-03-28 22:52:32 +0300

    Implemented adding a new weather card by user's request

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+import requests
 import sys
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/add', methods=['POST'])
+def add_city():
+    if request.form['city_name']:
+        city_name = request.form['city_name']
+    else:
+        city_name = ''
+    url = 'http://api.openweathermap.org/data/2.5/weather'
+    params = {'q': city_name,
+              'units': 'metric',
+              'appid': 'ccd0fe135c3a9971a1844fe5364d210c'}
+    res = requests.get(url, params=params)
+    weather_from_owa = res.json()
+    weather_info = {'city_name': city_name.upper(),
+                    'temp': round(weather_from_owa['main']['temp']),
+                    'weather_state': weather_from_owa['weather'][0]['main']}
+    return render_template('index.html', weather=weather_info)
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
